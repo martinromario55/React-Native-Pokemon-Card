90afc385efd7e2bdc4d4ca8f65fa215abc22b823
martinromario55 <martinromario55@gmail.com>
2024-02-03 07:32:42 +0300

    Add styles to the card

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -1,12 +1,33 @@
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet, Platform } from 'react-native'
 import React, { Component } from 'react'
 
 export default class PokemonCard extends Component {
   render() {
     return (
-      <View>
-        <Text>PokemonCard</Text>
+      <View style={styles.card}>
+        <Text>Pokemon Card</Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 16,
+    borderWidth: 2,
+    padding: 16,
+    margin: 16,
+    ...Platform.select({
+      ios: {
+        shadowColor: '#333',
+        shadowOffset: { width: 2, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+})
